930db876b5a5d5ee944cf39c78008ecfbdc2c238
thesakshamm <sakshamstha5514@gmail.com>
2026-02-14 11:15:13 +0545

    add date only column

--- request_script.py
+++ request_script.py
@@ -4,9 +4,6 @@ import requests
 url = 'https://api.openf1.org/v1/sessions?year=2026'
 response = requests.get(url)
 data = response.json()
-print(data)
 
-for i in data:
-    print(i)
-df = pd.DataFrame(data)
+
 
